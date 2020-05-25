41a1fedf148f585129472d6285b52364ed321340
Raphael <raph-topo@posteo.net>
2020-05-25 17:14:00 +0200

    Whitespace

--- src/Translatable.php
+++ src/Translatable.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package Mirai\WPlang
  */
@@ -10,222 +11,227 @@ namespace Mirai\WPlang;
  *
  * @package Mirai\WPlang
  */
-class Translatable {
+class Translatable
+{
 
-	/**
-	 * The package type: 'plugin', 'theme' or 'core'.
-	 *
-	 * @var string
-	 */
-	protected $type;
+    /**
+     * The package type: 'plugin', 'theme' or 'core'.
+     *
+     * @var string
+     */
+    protected $type;
 
-	/**
-	 * The plugin/theme slug, E.g. 'query-monitor'. In case of core, this is 'wordpress'.
-	 *
-	 * @var string
-	 */
-	protected $slug;
+    /**
+     * The plugin/theme slug, E.g. 'query-monitor'. In case of core, this is 'wordpress'.
+     *
+     * @var string
+     */
+    protected $slug;
 
-	/**
-	 * The package version.
-	 *
-	 * @var float|string
-	 */
-	protected $version;
+    /**
+     * The package version.
+     *
+     * @var float|string
+     */
+    protected $version;
 
-	/**
-	 * Array of the languages we are using.
-	 *
-	 * @var array
-	 */
-	protected $languages = [];
+    /**
+     * Array of the languages we are using.
+     *
+     * @var array
+     */
+    protected $languages = [];
 
-	/**
-	 * Full path to the language files target directory.
-	 *
-	 * @var string
-	 */
-	protected $wpLanguageDir;
+    /**
+     * Full path to the language files target directory.
+     *
+     * @var string
+     */
+    protected $wpLanguageDir;
 
-	/**
-	 * Array of translation packs available in our languages.
-	 *
-	 * @var array
-	 */
-	protected $translations = [];
+    /**
+     * Array of translation packs available in our languages.
+     *
+     * @var array
+     */
+    protected $translations = [];
 
-	/**
-	 * Constructor.
-	 *
-	 * @param string $type          The package type: 'plugin', 'theme' or 'core'.
-	 * @param string $slug          The plugin/theme slug, E.g. 'query-monitor'. In case of core, this is 'wordpress'.
-	 * @param string $version       The package version.
-	 * @param string $languages     Array of the languages we are using.
-	 * @param string $wpLanguageDir Full path to the language files target directory.
-	 */
-	public function __construct( $type, $slug, $version, $languages, $wpLanguageDir ) {
+    /**
+     * Constructor.
+     *
+     * @param string $type          The package type: 'plugin', 'theme' or 'core'.
+     * @param string $slug          The plugin/theme slug, E.g. 'query-monitor'. In case of core, this is 'wordpress'.
+     * @param string $version       The package version.
+     * @param string $languages     Array of the languages we are using.
+     * @param string $wpLanguageDir Full path to the language files target directory.
+     */
+    public function __construct($type, $slug, $version, $languages, $wpLanguageDir)
+    {
 
-		$this->type          = $type;
-		$this->slug          = $slug;
-		$this->version       = $version;
-		$this->languages     = $languages;
-		$this->wpLanguageDir = $wpLanguageDir;
+        $this->type          = $type;
+        $this->slug          = $slug;
+        $this->version       = $version;
+        $this->languages     = $languages;
+        $this->wpLanguageDir = $wpLanguageDir;
 
-		try {
-			$this->translations = $this->getAvailableTranslations();
-		} catch ( \Exception $e ) {
-			throw new \Exception( $e->getMessage() );
-		}
-	}
+        try {
+            $this->translations = $this->getAvailableTranslations();
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
 
-	/**
-	 * Get a list of available translations in our languages from the API.
-	 *
-	 * @throws \Exception
-	 * @return array
-	 */
-	protected function getAvailableTranslations() : array {
+    /**
+     * Get a list of available translations in our languages from the API.
+     *
+     * @throws \Exception
+     * @return array
+     */
+    protected function getAvailableTranslations(): array
+    {
 
-		switch ( $this->type ) {
-			case 'plugin':
-				$url = sprintf( 'https://api.wordpress.org/translations/plugins/1.0/?slug=%s&version=%s', $this->slug, $this->version );
-				break;
+        switch ($this->type) {
+            case 'plugin':
+                $url = sprintf('https://api.wordpress.org/translations/plugins/1.0/?slug=%s&version=%s', $this->slug, $this->version);
+                break;
 
-			case 'theme':
-				$url = sprintf( 'https://api.wordpress.org/translations/themes/1.0/?slug=%s&version=%s', $this->slug, $this->version );
-				break;
+            case 'theme':
+                $url = sprintf('https://api.wordpress.org/translations/themes/1.0/?slug=%s&version=%s', $this->slug, $this->version);
+                break;
 
-			case 'core':
-				$url = sprintf( 'https://api.wordpress.org/translations/core/1.0/?version=%s', $this->version );
-				break;
+            case 'core':
+                $url = sprintf('https://api.wordpress.org/translations/core/1.0/?version=%s', $this->version);
+                break;
 
-			default:
-				throw new \Exception( 'Unknown package type' );
-		}
+            default:
+                throw new \Exception('Unknown package type');
+        }
 
-		$body = file_get_contents( $url );
-		$res = json_decode( $body );
+        $body = file_get_contents($url);
+        $res = json_decode($body);
 
-		if ( ! isset( $res->translations ) || empty( $res->translations ) ) {
-			throw new \Exception( 'No translations found' );
-		}
+        if (!isset($res->translations) || empty($res->translations)) {
+            throw new \Exception('No translations found');
+        }
 
-		$translations = [];
+        $translations = [];
 
-		foreach ( $res->translations as $translation ) {
-			if ( in_array( $translation->language, $this->languages, true ) ) {
-				$translations[ $translation->language ][] = $translation;
-			}
-		}
+        foreach ($res->translations as $translation) {
+            if (in_array($translation->language, $this->languages, true)) {
+                $translations[$translation->language][] = $translation;
+            }
+        }
 
-		return $translations;
-	}
+        return $translations;
+    }
 
-	/**
-	 * Fetch the translations to our languages for our package.
-	 *
-	 * @return array
-	 */
-	public function fetch() : array {
+    /**
+     * Fetch the translations to our languages for our package.
+     *
+     * @return array
+     */
+    public function fetch(): array
+    {
 
-		$results = [];
+        $results = [];
 
-		foreach ( $this->translations as $language => $translation ) {
-			$result = $this->installTranslation( $translation[0]->package );
+        foreach ($this->translations as $language => $translation) {
+            $result = $this->installTranslation($translation[0]->package);
 
-			if ( $result ) {
-				$results[] = $language;
-			}
-		}
+            if ($result) {
+                $results[] = $language;
+            }
+        }
 
-		return $results;
-	}
+        return $results;
+    }
 
 
-	/**
-	 * Get the destination path for type type of object.
-	 *
-	 * This will also create the directory if if doesn't exist.
-	 *
-	 * @throws \Exception
-	 * @return string path to the destination directory.
-	 */
-	public function getDestPath() : string {
-		$destPath = $this->wpLanguageDir;
+    /**
+     * Get the destination path for type type of object.
+     *
+     * This will also create the directory if if doesn't exist.
+     *
+     * @throws \Exception
+     * @return string path to the destination directory.
+     */
+    public function getDestPath(): string
+    {
+        $destPath = $this->wpLanguageDir;
 
-		switch ( $this->type ) {
-			case 'plugin':
-				$destPath .= '/plugins';
-				break;
+        switch ($this->type) {
+            case 'plugin':
+                $destPath .= '/plugins';
+                break;
 
-			case 'theme':
-				$destPath .= '/themes';
-				break;
-		}
+            case 'theme':
+                $destPath .= '/themes';
+                break;
+        }
 
-		if ( ! is_dir( $destPath ) ) {
-			$result = mkdir( $destPath, 0775, true );
-			if ( ! $result ) {
-				throw new \Exception( 'Failed to create directory at: ' . $destPath );
-			}
-		}
+        if (!is_dir($destPath)) {
+            $result = mkdir($destPath, 0775, true);
+            if (!$result) {
+                throw new \Exception('Failed to create directory at: ' . $destPath);
+            }
+        }
 
-		return $destPath;
-	}
+        return $destPath;
+    }
 
-	/**
-	 * Unpack the downloaded translation ZIP file in the destination directory.
-	 *
-	 * @param string $tmpZipFileName Path to the translation ZIP file.
-	 * @return bool Whether the operation was successful or not.
-	 *
-	 * @throws \Exception
-	 */
-	public function unpackTranslation( $tmpZipFileName ) : bool {
+    /**
+     * Unpack the downloaded translation ZIP file in the destination directory.
+     *
+     * @param string $tmpZipFileName Path to the translation ZIP file.
+     * @return bool Whether the operation was successful or not.
+     *
+     * @throws \Exception
+     */
+    public function unpackTranslation($tmpZipFileName): bool
+    {
 
-		$result   = false;
-		$destPath = $this->getDestPath();
-		$zip      = new \ZipArchive();
+        $result   = false;
+        $destPath = $this->getDestPath();
+        $zip      = new \ZipArchive();
 
-		if ( true === $zip->open( $tmpZipFileName ) ) {
-			for ( $i = 0; $i < $zip->numFiles; $i++ ) {
-				$ok = $zip->extractTo( $destPath, [ $zip->getNameIndex( $i ) ] );
-				if ( false === $ok ) {
-					throw new \Exception( 'There was an error moving the translation to the destination directory' );
-				}
-			}
-			$zip->close();
+        if (true === $zip->open($tmpZipFileName)) {
+            for ($i = 0; $i < $zip->numFiles; $i++) {
+                $ok = $zip->extractTo($destPath, [$zip->getNameIndex($i)]);
+                if (false === $ok) {
+                    throw new \Exception('There was an error moving the translation to the destination directory');
+                }
+            }
+            $zip->close();
 
-			$result = true;
+            $result = true;
+        } else {
+            throw new \Exception('The was an error unzipping the translation files');
+        }
 
-		} else {
-			throw new \Exception( 'The was an error unzipping the translation files' );
-		}
+        return $result;
+    }
 
-		return $result;
-	}
+    /**
+     * Download and extract the translation ZIP file in our destination directory.
+     *
+     * @param string $packageUrl The URL to the translation package ZIP file.
+     * @return bool Whether the operation was successful or not.
+     */
+    public function installTranslation($packageUrl): bool
+    {
+        $result = false;
 
-	/**
-	 * Download and extract the translation ZIP file in our destination directory.
-	 *
-	 * @param string $packageUrl The URL to the translation package ZIP file.
-	 * @return bool Whether the operation was successful or not.
-	 */
-	public function installTranslation( $packageUrl ) : bool {
-		$result = false;
+        try {
+            $tmpZipFileName = sys_get_temp_dir() . '/' . $this->type . '-' . $this->slug . '-' . $this->version . '-' . basename($packageUrl);
+            $result = copy($packageUrl, $tmpZipFileName);
 
-		try {
-			$tmpZipFileName = sys_get_temp_dir() . '/' . $this->type . '-' . $this->slug . '-' . $this->version . '-' . basename( $packageUrl );
-			$result = copy( $packageUrl, $tmpZipFileName );
+            if ($result) {
+                $result = $this->unpackTranslation($tmpZipFileName);
+                unlink($tmpZipFileName);
+            }
+        } catch (\Exception $e) {
+        }
 
-			if ( $result ) {
-				$result = $this->unpackTranslation( $tmpZipFileName );
-				unlink( $tmpZipFileName );
-			}
-		} catch ( \Exception $e ) {
-		}
-
-		return $result;
-	}
-
+        return $result;
+    }
 }
--- src/Wplang.php
+++ src/Wplang.php
@@ -10,141 +10,144 @@ use Composer\Plugin\PluginInterface;
 use Composer\Installer\PackageEvent;
 use Composer\Package\PackageInterface;
 
-class Wplang implements PluginInterface, EventSubscriberInterface {
+class Wplang implements PluginInterface, EventSubscriberInterface
+{
 
-	/**
-	 * Array of the languages we are using.
-	 *
-	 * @var array
-	 */
-	protected $languages = [];
+    /**
+     * Array of the languages we are using.
+     *
+     * @var array
+     */
+    protected $languages = [];
 
-	/**
-	 * Full path to the language files target directory.
-	 *
-	 * @var string
-	 */
-	protected $wpLanguageDir = '';
+    /**
+     * Full path to the language files target directory.
+     *
+     * @var string
+     */
+    protected $wpLanguageDir = '';
 
-	/**
-	 * @var Composer
-	 */
-	protected $composer;
+    /**
+     * @var Composer
+     */
+    protected $composer;
 
-	/**
-	 * @var IOInterface
-	 */
-	protected $io;
+    /**
+     * @var IOInterface
+     */
+    protected $io;
 
-	/**
-	 * Composer plugin activation.
-	 */
-	public function activate( Composer $composer, IOInterface $io ) {
-		$this->composer = $composer;
-		$this->io = $io;
+    /**
+     * Composer plugin activation.
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
 
-		$extra = $this->composer->getPackage()->getExtra();
+        $extra = $this->composer->getPackage()->getExtra();
 
-		if ( ! empty( $extra['wordpress-languages'] ) ) {
-			$this->languages = $extra['wordpress-languages'];
-		}
+        if (!empty($extra['wordpress-languages'])) {
+            $this->languages = $extra['wordpress-languages'];
+        }
 
-		if ( ! empty( $extra['wordpress-language-dir'] ) ) {
-			$this->wpLanguageDir = dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) . '/' . $extra['wordpress-language-dir'];
-		}
-	}
+        if (!empty($extra['wordpress-language-dir'])) {
+            $this->wpLanguageDir = dirname(dirname(dirname(dirname(__DIR__)))) . '/' . $extra['wordpress-language-dir'];
+        }
+    }
 
 
-	/**
-	 * Subscribe to Composer events.
-	 *
-	 * @return array The events and callbacks.
-	 */
-	public static function getSubscribedEvents() {
-		return [
-			'post-package-install' => [
-				[ 'postPackageInstall', 0 ],
-			],
-			'post-package-update' => [
-				[ 'postPackageUpdate', 0 ],
-			],
-		];
-	}
+    /**
+     * Subscribe to Composer events.
+     *
+     * @return array The events and callbacks.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'post-package-install' => [
+                ['postPackageInstall', 0],
+            ],
+            'post-package-update' => [
+                ['postPackageUpdate', 0],
+            ],
+        ];
+    }
 
-	/**
-	 * Our callback for the post-package-install event.
-	 *
-	 * @param  PackageEvent $event The package event object.
-	 */
-	public function postPackageInstall( PackageEvent $event )
-	{
-		$package = $event->getOperation()->getPackage();
+    /**
+     * Our callback for the post-package-install event.
+     *
+     * @param  PackageEvent $event The package event object.
+     */
+    public function postPackageInstall(PackageEvent $event)
+    {
+        $package = $event->getOperation()->getPackage();
 
-		$this->getTranslations( $package );
-	}
+        $this->getTranslations($package);
+    }
 
-	/**
-	 * Our callback for the post-package-update event.
-	 *
-	 * @param  PackageEvent $event The package event object.
-	 */
-	public function postPackageUpdate( PackageEvent $event )
-	{
-		$package = $event->getOperation()->getTargetPackage();
+    /**
+     * Our callback for the post-package-update event.
+     *
+     * @param  PackageEvent $event The package event object.
+     */
+    public function postPackageUpdate(PackageEvent $event)
+    {
+        $package = $event->getOperation()->getTargetPackage();
 
-		$this->getTranslations( $package );
-	}
+        $this->getTranslations($package);
+    }
 
-	/**
-	 * Get translations for a package, where applicable.
-	 *
-	 * @param PackageInterface $package
-	 */
-	protected function getTranslations( PackageInterface $package ) {
+    /**
+     * Get translations for a package, where applicable.
+     *
+     * @param PackageInterface $package
+     */
+    protected function getTranslations(PackageInterface $package)
+    {
 
-		try {
-			$t = new \stdClass();
+        try {
+            $t = new \stdClass();
 
-			list( $provider, $name ) = explode( '/', $package->getName(), 2 );
+            list($provider, $name) = explode('/', $package->getName(), 2);
 
-			switch ( $package->getType() ) {
-				case 'wordpress-plugin':
-					$t = new Translatable( 'plugin', $name, $package->getVersion(), $this->languages, $this->wpLanguageDir );
-					break;
-				case 'wordpress-theme':
-					$t = new Translatable( 'theme', $name, $package->getVersion(), $this->languages, $this->wpLanguageDir );
-					break;
-				case 'package': case 'wordpress-core':
-					if ( 'roots' === $provider && 'wordpress' === $name ) {
-						$t = new Translatable( 'core', $name, $package->getVersion(), $this->languages, $this->wpLanguageDir );
-					}
-					break;
-				case 'wordpress-core':
-					if ( 'roots' === $provider && 'wordpress' === $name ) {
-						$t = new Translatable( 'core', $name, $package->getVersion(), $this->languages, $this->wpLanguageDir );
-					}
-					break;
+            switch ($package->getType()) {
+                case 'wordpress-plugin':
+                    $t = new Translatable('plugin', $name, $package->getVersion(), $this->languages, $this->wpLanguageDir);
+                    break;
+                case 'wordpress-theme':
+                    $t = new Translatable('theme', $name, $package->getVersion(), $this->languages, $this->wpLanguageDir);
+                    break;
+                case 'package':
+                case 'wordpress-core':
+                    if ('roots' === $provider && 'wordpress' === $name) {
+                        $t = new Translatable('core', $name, $package->getVersion(), $this->languages, $this->wpLanguageDir);
+                    }
+                    break;
+                case 'wordpress-core':
+                    if ('roots' === $provider && 'wordpress' === $name) {
+                        $t = new Translatable('core', $name, $package->getVersion(), $this->languages, $this->wpLanguageDir);
+                    }
+                    break;
 
-				default:
-					break;
-			}
+                default:
+                    break;
+            }
 
-			if ( is_a( $t, __NAMESPACE__ . '\Translatable' ) ) {
+            if (is_a($t, __NAMESPACE__ . '\Translatable')) {
 
-				$results = $t->fetch();
+                $results = $t->fetch();
 
-				if ( empty( $results ) ) {
-					$this->io->write( '      - ' . sprintf( 'No translations updated for %s', $package->getName() ) );
-				} else {
-					foreach ( $results as $result ) {
-						$this->io->write( '      - ' . sprintf( 'Updated translation to %1$s for %2$s', $result, $package->getName() ) );
-					}
-				}
-			}
-		} catch ( \Exception $e ) {
-			$this->io->writeError( 'ERROR: ' . $e->getMessage() );
-		}
-
-	}
-
+                if (empty($results)) {
+                    $this->io->write('      - ' . sprintf('No translations updated for %s', $package->getName()));
+                } else {
+                    foreach ($results as $result) {
+                        $this->io->write('      - ' . sprintf('Updated translation to %1$s for %2$s', $result, $package->getName()));
+                    }
+                }
+            }
+        } catch (\Exception $e) {
+            $this->io->writeError('ERROR: ' . $e->getMessage());
+        }
+    }
 }
